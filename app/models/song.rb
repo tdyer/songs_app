3859b9b37a49a67cb77aee6878d6226c3bb464aa
Tom Dyer <tdyer1@gmail.com>
2016-03-09 20:41:09 -0500

    Change the price validation, should only allow prices under 100

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
   validates :title, :uniqueness => true
   validates :price, :presence => true,
             :numericality => true,
-            :format => { :with => /^\d{1,4}(\.\d{0,2})?$/ }
+            :format => { :with => /\A\d{1,2}(\.\d{0,2})?\z/ }
   validates :duration, :numericality => { :only_integer => true}
 
 end
